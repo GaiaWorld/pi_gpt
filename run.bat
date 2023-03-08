ad1ad8ae3f1749235e296363667cc13cf4722507
moyy <412001799@qq.com>
2023-03-08 08:57:07 +0800

    feat: 改名 模板

--- run.bat
+++ run.bat
@@ -1,7 +1,7 @@
 @echo off
 
 if not exist config\config.json (
-    copy config\config_default.json config\config.json
+    copy config\config_template.json config\config.json
 )
 
 python ./src/bot.py
